af68fcfa1679e6dec47fab50ae5aa0dc3ad18982
Georgios Nikitas <georgiosnikitas@gmail.com>
2021-10-16 18:07:50 +0300

    housekeeing

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,14 +5,13 @@ public class HelloWorld {
     private static int result=0;
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-
         String whatToDO = "let's do some calculations";
         System.out.println(whatToDO);
         
         int x=137;
         int y=14;
         result=x+y;
+        
         System.out.println(x+"+"+y+"="+HelloWorld.getResult());
         
     }
