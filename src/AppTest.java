63ea939bc015d81a6c00939bc559d9a0e610ac30
Georgios Nikitas <georgiosnikitas@gmail.com>
2021-10-23 17:12:24 +0300

    refactor person and test birthdate

--- src/AppTest.java
+++ src/AppTest.java
@@ -1,16 +1,27 @@
 
 import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
+import java.time.LocalDate;
 import org.junit.Test;
 
 public class AppTest {
 
     @Test
-    public void test_JUnit() {
-        System.out.println("Testing Person");
+    public void test_Lastname() {
+        System.out.println("Testing person's lastname");
         String str1="Ken";
         assertEquals(new Person("Clark", "Ken", null).getLastname(), str1);
+    }
+
+    @Test
+    public void test_Age() {
+        System.out.println("Testing person's age");
+        Integer int1=10;
+        try {
+        int age = new Person("Clark", "Ken", LocalDate.of(2011,10,26)).getAge();
+            assertEquals(Integer.valueOf(age), int1);
+        } catch(Exception e) {
+            assertEquals(true, false);
+        }
     }
 
 }
