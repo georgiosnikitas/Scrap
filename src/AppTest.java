e8746289da1db188af0e424fab6e3c3ea714c199
Georgios Nikitas <georgiosnikitas@gmail.com>
2023-10-17 20:07:58 +0300

    updated testAge test

--- src/AppTest.java
+++ src/AppTest.java
@@ -5,16 +5,16 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void test_Lastname() {
+    public void testLastname() {
         System.out.println("Testing person's lastname");
         String str1="Ken";
         assertEquals(new Person("Clark", "Ken", null).getLastname(), str1);
     }
 
     @Test
-    public void test_Age() {
+    public void testAge() {
         System.out.println("Testing person's age");
-        Integer int1=10;
+        Integer int1=12;
         try {
         int age = new Person("Clark", "Ken", LocalDate.of(2011,10,26)).getAge();
             assertEquals(Integer.valueOf(age), int1);
